2fbdd4561a8cee9d644fe1e117308421561baf01
James Boullion <jboullion83@gmail.com>
2014-06-23 00:41:45 -0500

    Part 4

---
+++ Assets/Scripts/DestroyerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyerScript : MonoBehaviour {
+
+	void onTriggerEnter2D(Collider2D other){
+		if(other.tag == "Player"){
+			Debug.Break ();
+			return;
+		}
+
+		if(other.gameObject.transform.parent){
+			//destroy the parent object
+			Destroy (other.gameObject.transform.parent.gameObject);
+		}else{
+			//destroy the object
+			Destroy (other.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PowerUpScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpScript : MonoBehaviour {
+
+	HUDScript hud;
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if(other.tag == "Player"){
+			//may be inificiant
+			hud = GameObject.Find("Main Camera").GetComponent<HUDScript>();
+			
+			hud.UpdateScore(10);
+			Destroy(this.gameObject);
+		}
+	}	
+
+}
